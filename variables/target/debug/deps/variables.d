1d8393a9bb4312489ffd86bc227043859429cf04
98irisqiu <718956252@qq.com>
2024-11-27 11:26:11 +0800

    second commit

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+D:\data\coding files\Rust\Rust_test\variables\target\debug\deps\variables.exe: src/main.rs
+
+D:\data\coding files\Rust\Rust_test\variables\target\debug\deps\variables.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+D:\data\coding\ files\Rust\Rust_test\variables\target\debug\variables.exe: D:\data\coding\ files\Rust\Rust_test\variables\src\main.rs
